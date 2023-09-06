5f05adf55ac3b405794e73acedb76462d83be1e3
Glenn <glenn@pop-os.localdomain>
2023-09-06 12:03:55 -0700

    another initial change

---
+++ src/test/java/com/glenn/helloweb/HelloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenn.helloweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
